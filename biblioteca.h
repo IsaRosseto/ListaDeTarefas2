fc2d1c8fc541e612938c8db41a325c263b821af4
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-11-12 22:15:44 -0300

    trazendo do Clion para o git

--- biblioteca.h
+++ biblioteca.h
@@ -20,5 +20,14 @@ void lerTarefas(struct Tarefa tarefas[], int *nTarefas);
 void cadastrarTarefa(struct Tarefa tarefas[], int *nTarefas);
 void listarTarefas(struct Tarefa tarefas[], int nTarefas);
 void deletarTarefa(struct Tarefa tarefas[], int *nTarefas, int indice);
+void alterarTarefa(struct Tarefa tarefas[], int nTarefas);
+void filtrarPorPrioridade(struct Tarefa tarefas[], int nTarefas, int prioridade);
+void filtrarPorEstado(struct Tarefa tarefas[], int nTarefas, const char *estadoFiltrado);
+int compararPrioridadeDecrescente(const void *tarefaA, const void *tarefaB);
+void filtrarPorCategoria(struct Tarefa tarefas[], int nTarefas, const char *categoriaFiltrada);
+void filtrarPorPrioridadeECategoria(struct Tarefa tarefas[], int nTarefas, int prioridade, const char *categoria);
+void exportarPorPrioridade(struct Tarefa tarefas[], int nTarefas, int prioridade);
+void exportarPorCategoria(struct Tarefa tarefas[], int nTarefas, const char *categoria);
+void exportarPorPrioridadeECategoria(struct Tarefa tarefas[], int nTarefas, int prioridade, const char *categoria);
 
 #endif //PROJETO1_BIBLIOTECA_H
